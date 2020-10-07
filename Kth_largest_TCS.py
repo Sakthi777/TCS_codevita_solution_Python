7c1b5a5b654aec8fbd49335b7485d88709bee9ab
Sakthi777 <70153641+Sakthi777@users.noreply.github.com>
2020-10-07 19:14:56 +0530

    Update Kth_largest_TCS.py

--- Kth_largest_TCS.py
+++ Kth_largest_TCS.py
@@ -3,16 +3,19 @@ n=int(n)
 k=int(k)
 l=[]
 c=0
-"""for i in range(1,n+1):
+
+#---Method1---
+for i in range(1,n+1):
     if(n%i==0):
         l.append(i)
 print(l)
 if (k>len(l)):
     print(1)
 else:
-    print(l[-k])"""
+    print(l[-k])
 
 # other method
+
 for i in range(n,0,-1):
     if(n%i==0):
         c+=1
